f093f25ee4bed8f4b6ea5bd8d2f6af31652e1c6e
RONAN <sanskarkabra1306@gmail.com>
2020-07-21 11:00:13 +0530

    hello

---
+++ components/MainComponent.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import Menu from './MenuComponent';
+
+import {View,Platform} from 'react-native';
+import Dishdetail from './DishdetailComponent';
+import { createStackNavigator } from 'react-navigation';
+
+const MenuNavigator=createStackNavigator({
+    Menu: {screen:Menu},
+    Dishdetail:{screen:Dishdetail}
+},
+{
+    initialRouteName:'Menu',
+    navigationOptions:{
+        headerStyle:{
+            backgroundColor: '#000'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle:{
+            color:'#fff'
+        }
+    }
+})
+class Main extends Component{
+    
+    render(){
+        return(
+
+            <View style={{flex:1}}>
+            <MenuNavigator />
+        </View>
+        )
+    }
+}
+
+export default Main;
